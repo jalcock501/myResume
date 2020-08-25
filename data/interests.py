8ff378acef8a1c2377ea1332fb0baea66bccc64a
jalcock501 <jalcock501@googlemail.com>
2020-08-25 23:08:25 +0100

    initial fully working version

--- data/interests.py
+++ data/interests.py
@@ -7,10 +7,10 @@ def get_interest_data():
 
     interests = {
         'text1': 'My personal interests include basketball, football, Muay Thai, Krav Maga \
-                  and regularly going to the gym. I also enjoy swimming, snowboarding and skiing. \
+                  and regularly going to the gym. I also enjoy swimming, hiking and camping. \
                   In my spare time I develop my own programs and apps using C# and Unity3D. \
                   I have a keen interest in computers, programming and technology, which \
-                  motivated me to make my career in this area.'
+                  motivated me to make my career in this area.',
     }
 
     return interests
